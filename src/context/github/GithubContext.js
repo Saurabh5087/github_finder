5d9ac0b382e7ac5df4cf1b85553cbfa4c593df6a
saurabh-kd-singh <sksingh5087@gmail.com>
2023-05-17 19:52:12 +0530

    Creating GithubReducer to manage states present in GithubContext

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext()
 const GITHUB_URL = "https://api.github.com"
@@ -7,24 +8,29 @@ const GITHUB_URL = "https://api.github.com"
 // wrap around those components(Children) who requires
 // the states defined in this provider function
 export const GithubProvider = ({children}) => {
-  
-  // users state is used to store all the user fecthed on page load
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  const intialState = {
+    users: [],
+    loading: true
+  }
+
+  const [state, dispatch] = useReducer(githubReducer, intialState)
 
   // Fetch User function to get the users array from GithHub API 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`)
     const data = await response.json()
-    setUsers(data)
-    setLoading(false)
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: data
+    })
   }
 
   return(
     <GithubContext.Provider 
     value={{ 
-      users,
-      loading,
+      users: state.users,
+      loading: state.loading,
       fetchUsers
     }}>
       {children}
