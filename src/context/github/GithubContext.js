0aec519a20290ac1d728a2e3ed30a59e94b3aba6
saurabh-kd-singh <sksingh5087@gmail.com>
2023-05-18 17:56:48 +0530

    Moving getUsers and getUserRepos to Action file

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -2,7 +2,6 @@ import { createContext, useReducer } from 'react';
 import githubReducer from './GithubReducer';
 
 const GithubContext = createContext()
-const GITHUB_URL = "https://api.github.com"
 
 // Creating and Exporting a Provider function which will
 // wrap around those components(Children) who requires
@@ -17,62 +16,11 @@ export const GithubProvider = ({children}) => {
 
   const [state, dispatch] = useReducer(githubReducer, intialState)
 
-  // Get single user results
-  const getUser = async (login) => {
-    setLoading()
-    const response = await fetch(`${GITHUB_URL}/users/${login}`)
-
-    if (response.status === 404) {
-      window.location = '/notfound'
-    } else {
-      const data = await response.json()
-      dispatch({
-        type: 'GET_USER',
-        payload: data
-      })
-    }
-  }
-
-
-  // Get user repos
-  const getUserRepos = async (login) => {
-    const params = new URLSearchParams({
-      sort: 'created',
-      per_page: 10
-    })
-
-    setLoading()
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)
-    const data = await response.json()
-    dispatch({
-      type: 'GET_REPOS',
-      payload: data
-    })
-  }
-
-
-  // Clear users from state
-  const clearUsers = () => {
-    dispatch({
-      type: 'CLEAR_USER'
-    })
-  }
-
-  // Set Loading
-  const setLoading = () => {
-    dispatch({
-      type: 'SET_LOADING'
-    })
-  }
-
   return(
     <GithubContext.Provider 
     value={{ 
       ...state,
-      dispatch,
-      getUser,
-      clearUsers,
-      getUserRepos
+      dispatch
     }}>
       {children}
     </GithubContext.Provider>
