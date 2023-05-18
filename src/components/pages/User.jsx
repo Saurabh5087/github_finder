84b5e1697803fd172761658cf47ef6057ef8bce0
saurabh-kd-singh <sksingh5087@gmail.com>
2023-05-18 16:21:48 +0530

    Fetch and Show: Single user details
    Profile Image, Username, Bio other stats

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -1,17 +1,81 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { useEffect, useContext } from "react";
+import Spinner from '../layout/Spinner'
 import GithubContext from "../../context/github/GithubContext";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function User() {
-  const { getUser, user } = useContext(GithubContext)
+  const { getUser, user, loading } = useContext(GithubContext)
   const params = useParams()
 
   useEffect(() => {
     getUser(params.login)
   }, [])
 
+  // Destructing all the variables we need 
+  // provided by the Github API for a user login
+  const {
+    name, type, avatar_url, location, bio, blog, twitter_username, login,
+    html_url, followers, following, public_repos, public_gists, hireable
+  } = user
+
+  if (loading) {
+    return <Spinner />
+  }
+
   return (
-    <div>{user.login}</div>
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+
+        <div className="mb-4">
+          <Link to='/' className='btn btn-ghost'>Back To Search</Link>
+        </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+
+          {/* Profile Image, Username, Login_Name */}
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="Profile_Photo" />
+              </figure>
+              <div className="card-body flex flex-cols justify-end">
+                <div>
+                  <h2 className="card-title mb-0">{name}</h2>
+                  <p>{login}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Right Side of profile Image */}
+          {/* Username, bio, Github link, other Stats */}
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                {name}
+                <div className="ml-2 mr-1 badge badge-success">
+                  {type}
+                </div>
+                {hireable && (
+                  <div className="mx-1 badge badge-info">
+                    Hireable
+                  </div>
+                )}
+              </h1>
+              <p className='mt-1'>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a href={html_url} target='_blank' rel='noreferrer' className='btn btn-outline'>
+                  Visit GitHub Profile
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </>
   )
 }
 
