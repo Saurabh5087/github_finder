8227ea247a73ddf0ec775773535781c0170befcb
saurabh-kd-singh <sksingh5087@gmail.com>
2023-05-18 18:11:03 +0530

    Clear Button functionality Fixed

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -61,7 +61,7 @@ function UserSearch() {
       {/* Clear button */}
       {users.length > 0 && (
         <div>
-          <button onClick={() => dispatch({type: 'CLEAR_USERS'})} className="btn btn-ghost btn-lg">
+          <button onClick={() => dispatch({type: 'CLEAR_USER'})} className="btn btn-ghost btn-lg">
             Clear
           </button>
         </div>
